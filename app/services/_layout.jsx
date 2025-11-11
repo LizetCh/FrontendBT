1720217b98142baebe71a5c204090551ab2e261b
Lizet Lopez <lizet.chlopez@gmail.com>
2025-11-10 18:58:29 -0600

    Added serviceItem and ServiceList component

---
+++ app/services/_layout.jsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router"
+
+const ServiceLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  )
+}
+
+export default ServiceLayout
--- app/services/index.jsx
+++ app/services/index.jsx
@@ -1,9 +1,29 @@
+import ServicesList from '@/components/services/ServicesList'
 import { useState } from 'react'
+import { View } from 'react-native'
 
 const ServiceScreen = () => {
 
-  const [services, setServices] = useState([])
+  const [services, setServices] = useState([
+    { id: 1, title: 'Clases de guitarra', description: 'Aprende a tocar la guitarra', category: 'Música', hours: 3, contact: '989898989', location: 'Merida, Yuc' }
+  ])
 
+
+
+
+
+  return (
+    <View>
+      <ServicesList services={services}/>
+
+    </View>
+  )
 
 }
+
+export default ServiceScreen
+
+
+
+
 
