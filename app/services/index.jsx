cfc6a090259457471c8c4210db680a771c14caed
maureen-tellez <maureentellez0816@gmail.com>
2025-11-11 19:17:56 -0600

    index??

--- app/services/index.jsx
+++ app/services/index.jsx
@@ -1,9 +1,27 @@
-import { useState } from 'react'
+import { router } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+const loginScreen = () => {
 
-const ServiceScreen = () => {
-
-  const [services, setServices] = useState([])
-
+  return (
+    <View style = {styles.container}   >
+    
+      <Text style={styles.titulo}>Banco de Tiempo</Text>
+      <input type="text" placeholder="Enter text here" />
+      <TouchableOpacity
+        style={styles.buton}
+       onPress={()=>router.push('./notes')}
+      >
+        <Text style={{color:'white', fontSize:18,}}>Miua</Text>
+      </TouchableOpacity>
+    </View>
+  );
 
 }
+const styles = StyleSheet.create({
 
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
