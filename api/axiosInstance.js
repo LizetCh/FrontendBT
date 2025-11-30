a6de7097a758a40b867fb1bdd05eb03a0a7cea7b
Lizet Lopez <lizet.chlopez@gmail.com>
2025-11-30 15:43:52 -0600

    cambiar URL a render

--- api/axiosInstance.js
+++ api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  //baseURL: "https://backendbt-0qxn.onrender.com/api" //url de render 
-  baseURL: "http://localhost:8080/api" //url local
+  baseURL: "https://backendbt-0qxn.onrender.com/api" //url de render 
+  //baseURL: "http://localhost:8080/api" //url local
 });
